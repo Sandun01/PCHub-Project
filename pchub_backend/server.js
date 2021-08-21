0b65f7e43b76353c72d6816545bbd439c6950ccf
nilanmeegoda99 <nilanmeegoda@gmail.com>
2021-08-21 22:12:42 +0530

    merge issue

--- pchub_backend/server.js
+++ pchub_backend/server.js
@@ -1,30 +1,30 @@
 import express from 'express'
-import bodyParser from 'body-parser'
 import dotenv from 'dotenv'
-import cors from 'cors'
-
-import connectDB from './src/config/db.js'
+import colors from 'colors'
+import connectDB from './config/db.js'
 
 dotenv.config()
 
-const app = express()
-app.use(cors())
-app.use(bodyParser.json())
-
-//connect to the database
 connectDB()
 
-const PORT = process.env.PORT || 5000;
+const app = express()
+
+//a middleware to accpet json request body through the server 
+app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.send('Api is working')
-});
+  res.send('The API is working')
+})
+
+app.use(notFound)
+app.use(errorHandler)
+
+//calling the dotenv file
+const PORT = process.env.PORT || 5000
 
 app.listen(
   PORT,
   console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
   )
 )
-
-
