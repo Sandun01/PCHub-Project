dd1af00ecbbc3ef2148be8d5b4d752462177e45c
nilanmeegoda99 <nilanmeegoda@gmail.com>
2021-08-21 22:13:20 +0530

    merge issue 2

--- pchub_backend/server.js
+++ pchub_backend/server.js
@@ -2,6 +2,11 @@ import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
 import connectDB from './config/db.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+
+import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
+import orderRoutes from './routes/orderRoutes.js'
 
 dotenv.config()
 
@@ -15,6 +20,11 @@ app.use(express.json())
 app.get('/', (req, res) => {
   res.send('The API is working')
 })
+
+//Routing to relavent Roters
+app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
+app.use('/api/orders', orderRoutes)
 
 app.use(notFound)
 app.use(errorHandler)
