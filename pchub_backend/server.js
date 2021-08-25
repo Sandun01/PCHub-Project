76f87202a3f0174ecc24ff8892d57c237313f147
Sandun01 <sandunlakshitha06@gmail.com>
2021-08-26 02:34:46 +0530

    fix1

--- pchub_backend/server.js
+++ pchub_backend/server.js
@@ -22,8 +22,8 @@ app.use(bodyParser.json());
 
 app.use('/api/auth', auth);
 app.use('/api/users', authRoutes);
-app.use('/api/wishlist', WishlistRouts);
-app.use('/api/product', ProductRouts);
+app.use('/api/wishlists', WishlistRouts);
+app.use('/api/products', ProductRouts);
 app.use('/api/orders', OrderRouts);
 
 const PORT = process.env.PORT || 5000;
