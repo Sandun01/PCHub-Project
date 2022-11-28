55d4d9cbd15e059919a3357b41330742a4abca30
Sandun01 <sandunlakshitha06@gmail.com>
2022-11-28 20:11:20 +0530

    final 1

--- backend/server.js
+++ backend/server.js
@@ -27,6 +27,7 @@ app.use('/api/products', ProductRouts);
 app.use('/api/orders', OrderRouts);
 
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve()
 
 if(process.env.NODE_ENV === 'production'){
   app.use(express.static(path.join(__dirname, '/frontend/build')))
