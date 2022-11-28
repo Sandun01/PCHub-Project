8e13da84bc1f0d664e0be9a92990a4deb4c00843
Sandun01 <sandunlakshitha06@gmail.com>
2022-11-28 20:16:16 +0530

    update 4

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import path from 'path'
 
 import connectDB from './src/config/db.js';
 
